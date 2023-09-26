8202bcfc6a23f792201a35ea9deffe52793e5c68
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2023-09-26 23:35:02 +0300

    feat: add customer info

--- lib/widgets/shopping_cart_tab.dart
+++ lib/widgets/shopping_cart_tab.dart
@@ -1,4 +1,6 @@
+import 'package:exo/model/app_state_model.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 class ShoppingCartTab extends StatefulWidget {
   const ShoppingCartTab({super.key});
@@ -10,12 +12,129 @@ class ShoppingCartTab extends StatefulWidget {
 }
 
 class _ShoppingCartTabState extends State<ShoppingCartTab> {
+  String? name;
+  String? email;
+  String? phone;
+  String? pin;
+  DateTime dateTime = DateTime.now();
+
+  Widget _buildNameField() {
+    return CupertinoTextField(
+      prefix: const Icon(
+        CupertinoIcons.person_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      textCapitalization: TextCapitalization.words,
+      autocorrect: false,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Name',
+      onChanged: (newName) {
+        name = newName;
+      },
+    );
+  }
+
+  Widget _buildEmailField() {
+    return const CupertinoTextField(
+      prefix: Icon(
+        CupertinoIcons.mail_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Email',
+    );
+  }
+
+  Widget _buildLocationField() {
+    return CupertinoTextField(
+      prefix: const Icon(
+        CupertinoIcons.location_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Location',
+    );
+  }
+
+  SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(
+      AppStateModel model) {
+    return SliverChildBuilderDelegate(
+      (context, index) {
+        switch (index) {
+          case 0:
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: _buildNameField(),
+            );
+          case 1:
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: _buildEmailField(),
+            );
+          case 2:
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: _buildLocationField(),
+            );
+          default:
+          // Do nothing. For now.
+        }
+        return null;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(slivers: <Widget>[
-      CupertinoSliverNavigationBar(
-        largeTitle: Text('Cart'),
-      ),
-    ]);
+    return Consumer<AppStateModel>(
+      builder: (context, model, child) {
+        return CustomScrollView(
+          slivers: <Widget>[
+            const CupertinoSliverNavigationBar(
+              largeTitle: Text('Shopping Cart'),
+            ),
+            SliverSafeArea(
+              top: false,
+              minimum: const EdgeInsets.only(top: 4),
+              sliver: SliverList(
+                delegate: _buildSliverChildBuilderDelegate(model),
+              ),
+            )
+          ],
+        );
+      },
+    );
   }
 }
